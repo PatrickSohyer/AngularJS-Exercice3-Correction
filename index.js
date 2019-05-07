75535c6b19ada166f3074b2ab432b90ee99e9f3c
PatrickSohyer <p.sohyer@gmail.com>
2019-05-07 11:30:56 +0200

    Partie 2

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ myApp.controller('formCtrl', ['$scope', function($scope) {
   // je scope regexName et je rentre la regex qui n'autorise pas les caractères spéciaux les chiffres et les prénoms de moins deux lettres et quinze lettres max
   $scope.regexName = /^[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ']{2,26}[- ']?[a-zA-ZéèÉÈôîêûÛÊÔÎùÙïöëüËÏÖÜ]{0,26}$/;
   $scope.regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  $scope.mailInfos = []; // création du tableau
+
+  $scope.storeData = function() { // création de la function storeData qui insère les données des inputs
+    $scope.mailInfos.push({ //
+      name: $scope.name,
+      email: $scope.email,
+      topic: $scope.topic,
+      textArea: $scope.textArea
+    });
+  }
 }]);
